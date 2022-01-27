f1aeb13bb9dcc538e04aac62bec9c5c0eb5b5b98
Михаил Гурин <mikhail.gurin@gmail.com>
2022-01-27 20:47:07 +0300

    step 7

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,17 @@
+"""Игра проверка на чётность."""
+from math import gcd
+from random import randint
+
+from cli import process_game
+
+
+def get_conditions():
+    number_one = randint(1, 100)
+    number_two = randint(1, 100)
+    question = '{} {}'.format(number_one, number_two)
+    return question, str(gcd(number_one, number_two))
+
+
+def start():
+    header = 'Find the greatest common divisor of given numbers.'
+    process_game(header, get_conditions)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Игра найти НОД."""
+
+from games.gcd import start
+
+
+def main():
+    """Основная функция."""
+    start()
+
+
+if __name__ == '__main__':
+    main()
