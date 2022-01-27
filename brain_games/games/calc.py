54208e5ba0eb877661167b5f47d6ca8733e21ba5
Михаил Гурин <mikhail.gurin@gmail.com>
2022-01-27 20:38:11 +0300

    step 6

---
+++ brain_games/games/calc.py
@@ -0,0 +1,17 @@
+"""Игра проверка на чётность."""
+from random import randint, choice
+
+from cli import process_game
+
+
+def get_conditions():
+    number_one = randint(1, 100)
+    number_two = randint(1, 100)
+    sign = choice(['+', '*', '-'])
+    question = '{} {} {}'.format(number_one, sign, number_two)
+    return question, str(eval(question))
+
+
+def start():
+    header = 'What is the result of the expression?'
+    process_game(header, get_conditions)
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Игра калькулятор."""
+
+from games.calc import start
+
+
+def main():
+    """Основная функция."""
+    start()
+
+
+if __name__ == '__main__':
+    main()
